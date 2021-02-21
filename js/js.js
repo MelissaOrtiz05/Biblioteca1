7ace0254699f8c4733640ad7a4bc6e62901cbbad
MelissaOrtiz05 <77902578+MelissaOrtiz05@users.noreply.github.com>
2021-02-21 08:17:15 -0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function ocultar(){
+    var div=document.getElementById('imgN');
+    div.style.display='none';
+}
+
+function mostrar(){
+    var div=document.getElementById('imgN');
+    div.style.display='';
+}
